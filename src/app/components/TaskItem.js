39afcf0f48ef7ec3e7b47648afd79815ff83dfc8
binod bastola <bndbstl@gmail.com>
2025-03-06 13:36:23 +0545

    slight change in taskitem component

--- src/app/components/TaskItem.js
+++ src/app/components/TaskItem.js
@@ -1,17 +1,37 @@
 const TaskItem = ({ task, onUpdate, onDelete }) => {
     return (
       <div className="flex justify-between p-4 border-b">
-        <span className={task.completed || task.status === 'completed'? "line-through text-gray-500" : ""}>
+        <span
+          className={
+            task.completed || task.status === "completed"
+              ? "line-through text-gray-500"
+              : ""
+          }
+        >
           {task.name} ({task.category})
         </span>
         <div className="flex gap-2">
-          {(
-            <button onClick={() => onUpdate(task._id)}
-             className={ task.status === 'completed'|| task.completed ? "bg-green-500 text-white p-2" : task.status==='pending' ? " bg-yellow-500 text-white p-2" :""}>
-            {task.status === 'completed' || task.completed ? " Completed" : " Complete"}
+          {
+            <button
+              disabled={task.completed || task.status === "completed"}
+              onClick={() => onUpdate(task._id)}
+              className={
+                task.status === "completed" || task.com
+                  ? "bg-green-500 text-white p-2"
+                  : task.status === "pending"
+                  ? " bg-yellow-500 text-white p-2"
+                  : ""
+              }
+            >
+              {task.status === "completed" || task.completed
+                ? " Completed"
+                : " Complete"}
             </button>
-          )}
-          <button onClick={() => onDelete(task._id)} className="bg-red-500 text-white p-2">
+          }
+          <button
+            onClick={() => onDelete(task._id)}
+            className="bg-red-500 text-white p-2"
+          >
             Delete
           </button>
         </div>
